1ba3a4a5fc2e16423a83be6be133a01fd8fb84a8
yulianhp <kono.utsukushi24@gmail.com>
2017-12-14 20:29:31 +0700

    persiapan sebelum rebase

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
-const homeController = require('../controller/home')
+const facebook = require('../models/fbmodel.js');
+// const homeController = require('../controller/home')
 
 /* GET home page. */
-// router.get('/', homeController.findAll);
+router.get('/', facebook.getUserData);
 // router.post('/',homeController.createItem);
 // router.delete('/',destroyItem)
 
