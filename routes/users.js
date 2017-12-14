5aac2d82c21c98a48bd2907f90009f1d360a6a53
febriliando <febriliandoputra@gmail.com>
2017-12-14 18:03:54 +0700

    ready to work

---
+++ routes/users.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+const userController = require('../controller/user')
+
+/* GET home page. */
+// router.get('/', userController.getUser);
+// router.post('/',userController.createUser);
+// router.delete('/',destroyUser)
+
+module.exports = router;
+// 
